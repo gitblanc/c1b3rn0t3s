3e23c930bc874e58422680e4ba773d573ac731d4
Eduardo Blanco Bielsa <gitblanc@tutanota.com>
2024-03-13 15:17:33 +0100

    Quartz sync: Mar 13, 2024, 3:17 PM

--- quartz/util/path.ts
+++ quartz/util/path.ts
@@ -200,6 +200,36 @@ export interface TransformOptions {
   allSlugs: FullSlug[]
 }
 
+// export function transformLink(src: FullSlug, target: string, opts: TransformOptions): RelativeURL {
+//   let targetSlug = transformInternalLink(target)
+
+//   if (opts.strategy === "relative") {
+//     return targetSlug as RelativeURL
+//   } else {
+//     const folderTail = isFolderPath(targetSlug) ? "/" : ""
+//     const canonicalSlug = stripSlashes(targetSlug.slice(".".length))
+//     let [targetCanonical, targetAnchor] = splitAnchor(canonicalSlug)
+
+//     if (opts.strategy === "shortest") {
+//       // if the file name is unique, then it's just the filename
+//       const matchingFileNames = opts.allSlugs.filter((slug) => {
+//         const parts = slug.split("/")
+//         const fileName = parts.at(-1)
+//         return targetCanonical === fileName
+//       })
+
+//       // only match, just use it
+//       if (matchingFileNames.length === 1) {
+//         const targetSlug = matchingFileNames[0]
+//         return (resolveRelative(src, targetSlug) + targetAnchor) as RelativeURL
+//       }
+//     }
+
+//     // if it's not unique, then it's the absolute path from the vault root
+//     return (joinSegments(pathToRoot(src), canonicalSlug) + folderTail) as RelativeURL
+//   }
+// }
+
 export function transformLink(src: FullSlug, target: string, opts: TransformOptions): RelativeURL {
   let targetSlug = transformInternalLink(target)
 
