54efd0c4f3bbae85b88f3c16a9b7b98fd843023c
Eduardo Blanco Bielsa <87705461+gitblanc@users.noreply.github.com>
2024-06-02 21:09:27 +0000

    buy me a cofee

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -34,7 +34,7 @@ export default ((opts?: Options) => {
             </li>
           ))}
         </ul>
-        <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="gitblanc" data-description="Support me on Buy me a coffee!" data-message="Thank you for supporting me! You're awesome :)" data-color="#FF5F5F" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
+        <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="gitblanc" data-description="Support me on Buy me a coffee!" data-message="" data-color="#FF5F5F" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
       </footer>
     )
   }
