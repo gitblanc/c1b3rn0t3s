4a45e2d2b6c09e6d83c26bea301974f34b097606
gitblanc <gitblanc@tutanota.com>
2024-08-27 15:00:24 +0200

    prueba icon

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -3,47 +3,47 @@ import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } fro
 import { classNames } from "../util/lang"
 import { i18n } from "../i18n"
 
-// const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzComponentProps) => {
-//   const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
-//   const baseDir = pathToRoot(fileData.slug!)
-//   return (
-//     <h1 class={classNames(displayClass, "page-title")}>
-//       <a href={baseDir}>{title}</a>
-//     </h1>
-//   )
-// }
-
 const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzComponentProps) => {
   const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
   const baseDir = pathToRoot(fileData.slug!)
-  const iconPath = joinSegments(baseDir, "static/icon.png")
   return (
-    <h2 class={classNames(displayClass, "page-title")}>
-      <a href={baseDir}>
-        <img class="Logo" src={iconPath} alt={title}/>
-      </a>
-    </h2>
+    <h1 class={classNames(displayClass, "page-title")}>
+      <a href={baseDir}>{title}</a>
+    </h1>
   )
 }
 
-// PageTitle.css = `
-// .page-title {
-//   margin: 0;
+// const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzComponentProps) => {
+//   const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
+//   const baseDir = pathToRoot(fileData.slug!)
+//   const iconPath = joinSegments(baseDir, "static/icon.png")
+//   return (
+//     <h2 class={classNames(displayClass, "page-title")}>
+//       <a href={baseDir}>
+//         <img class="Logo" src={iconPath} alt={title}/>
+//       </a>
+//     </h2>
+//   )
 // }
-// `
 
 PageTitle.css = `
 .page-title {
-  font-size: 1.75rem;
-  margin: 0;
-}
-.Logo {
-  max-height: 195px;
-  min-height: 50px;
-  max-width: 212px;
-  min-width: 50px;
   margin: 0;
 }
 `
+
+// PageTitle.css = `
+// .page-title {
+//   font-size: 1.75rem;
+//   margin: 0;
+// }
+// .Logo {
+//   max-height: 195px;
+//   min-height: 50px;
+//   max-width: 212px;
+//   min-width: 50px;
+//   margin: 0;
+// }
+// `
 
 export default (() => PageTitle) satisfies QuartzComponentConstructor
