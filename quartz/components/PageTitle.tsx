cbc2f67a26fbc2310ecddff3b86c0d6f79cbf042
gitblanc <gitblanc@tutanota.com>
2024-08-27 14:38:44 +0200

    prueba icon

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -1,20 +1,47 @@
-import { pathToRoot } from "../util/path"
+import { joinSegments, pathToRoot } from "../util/path"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import { classNames } from "../util/lang"
 import { i18n } from "../i18n"
 
+// const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzComponentProps) => {
+//   const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
+//   const baseDir = pathToRoot(fileData.slug!)
+//   return (
+//     <h1 class={classNames(displayClass, "page-title")}>
+//       <a href={baseDir}>{title}</a>
+//     </h1>
+//   )
+// }
+
 const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzComponentProps) => {
   const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
   const baseDir = pathToRoot(fileData.slug!)
+  const iconPath = joinSegments(baseDir, "static/icon.png")
   return (
-    <h1 class={classNames(displayClass, "page-title")}>
-      <a href={baseDir}>{title}</a>
-    </h1>
+    <h2 class={classNames(displayClass, "page-title")}>
+      <a href={baseDir}>
+        <img class="Logo" src={iconPath} alt={title}/>
+      </a>
+    </h2>
   )
 }
 
+// PageTitle.css = `
+// .page-title {
+//   margin: 0;
+// }
+// `
+
 PageTitle.css = `
 .page-title {
+  font-size: 1.75rem;
+  margin: 0;
+}
+.Logo {
+  max-height: 195px;
+  min-height: 50px;
+  max-width: 212px;
+  min-width: 50px;
   margin: 0;
 }
 `
