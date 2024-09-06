bbe69b246a2c1a2f61a70cf8a3523cc615ded3b8
gitblanc <gitblanc@tutanota.com>
2024-09-06 15:21:26 +0200

    button

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -34,7 +34,8 @@ export default ((opts?: Options) => {
             </li>
           ))}
         </ul>
-        <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="gitblanc" data-description="Support me on Buy me a coffee!" data-message="" data-color="#F44336" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
+
+        <script data-name="BMC-Widget" data-cfasync="true" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="gitblanc" data-description="Support me on Buy me a coffee!" data-message="" data-color="#F44336" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
       </footer>
     )
   }
